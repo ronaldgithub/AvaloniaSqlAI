5e6113eef181f7ea604069f87eed085d970757f7
Ronald de Groot <Ronald.de.Groot@live.nl>
2026-05-21 11:13:15 +0200

    Update badges test script and add screenshot

--- scripts/tests/badges_missing_index.sql
+++ scripts/tests/badges_missing_index.sql
@@ -12,3 +12,19 @@ SELECT [Id]
 Missing Index (Impact 99.9922): 
 CREATE NONCLUSTERED INDEX [<Name of Missing Index, sysname,>] ON [dbo].[Badges] ([UserId])
 */
+
+
+
+/* The result should look like this after AI analysis and index creation:
+
+CREATE NONCLUSTERED INDEX [IX_Badges_UserId] ON [dbo].[Badges]
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF
+, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF
+, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON
+, FILLFACTOR = 100, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) 
+ON [PRIMARY]
+GO
+
+*/
